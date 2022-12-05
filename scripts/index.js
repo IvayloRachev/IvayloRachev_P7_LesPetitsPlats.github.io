c2f5edeae79761bae5fa18bb8360ba138db25c8f
Ivaylo Rachev <rachev.ivaylo@yahoo.fr>
2022-12-05 13:51:30 +0100

    Debug errors
    Résolution des erreurs

--- scripts/index.js
+++ scripts/index.js
@@ -2,19 +2,11 @@
 function displayRecipe(inputArray) {
     const cardsSection = document.querySelector('.cards');
     cardsSection.innerHTML = "";
-
-    if (inputArray.length == 0) {
-        document.querySelector('.search_error').style.display = "block";
-    }else{
-
         inputArray.forEach((recipe) => {
-            document.querySelector('.search_error').style.display = "none";
             const recipeModel = recipesFactory(recipe);
             const recipeCardDOM = recipeModel.getRecipeCardDOM();
             cardsSection.appendChild(recipeCardDOM);
         });
-
-    }
 }
 displayRecipe(recipes);
 //fin de display recipes
--- scripts/search.js
+++ scripts/search.js
@@ -2,56 +2,37 @@ function globalSearch() {
     const inputArray = [];
     let searchField = document.getElementById('search').value;
 
-    if (searchField.length < 3 || searchField.length == "") {
+    if (searchField.length < 3) {
         document.querySelector('.search_error').style.display = "block";
-        document.querySelector('.search_error').style.display = "";
         displayRecipe(recipes);
-        return false;
     }else {
         document.querySelector('.search_error').style.display = "none";
 
         for (let i = 0; i < recipes.length; i++) {
-            let doublon = false;
-
-            let description = recipes[i].description;
-            if (description.toLowerCase().includes(searchField.toLowerCase()) && doublon == false) {
-                inputArray.push(recipes[i]);
-                doublon = true;
-            }
+            let displaySort = false;
 
             let names = recipes[i].name;
-            if (names.toLowerCase().includes(searchField.toLowerCase()) && doublon == false) {
+            if (names.toLowerCase().includes(searchField.toLowerCase()) && displaySort == false) {
                 inputArray.push(recipes[i]);
-                doublon = true;
+                displaySort = true;
             }
 
             let ingredients = recipes[i].ingredients;
             for (let i = 0; i < ingredients.length; i++) {
                 let ingredientsList = ingredients[i].ingredient;
-                if (ingredientsList.toLowerCase().includes(searchField.toLowerCase()) && doublon == false) {
+                if (ingredientsList.toLowerCase().includes(searchField.toLowerCase()) && displaySort == false) {
                     inputArray.push(recipes[i]);
-                    doublon = true;
+                    displaySort = true;
                 }
             }
 
-            let appliances = recipes[i].appliance;
-            if (appliances.toLowerCase().includes(searchField.toLowerCase()) && doublon == false) {
+            let description = recipes[i].description;
+            if (description.toLowerCase().includes(searchField.toLowerCase()) && displaySort == false) {
                 inputArray.push(recipes[i]);
-                doublon = true;
+                displaySort = true;
             }
-
-            let ustensils = recipes[i].ustensils;
-            for (let i = 0; i < ustensils.length; i++) {
-                let ustensilsList = ustensils[i];
-                if (ustensilsList.toLowerCase().includes(searchField.toLowerCase()) && doublon == false) {
-                    inputArray.push(recipes[i]);
-                    doublon = true;
-                }
-            }
-
         };
         displayRecipe(inputArray);
-        return true;
     }
 }
 
@@ -59,10 +40,4 @@ let buttonSearch = document.querySelector('button');
 buttonSearch.addEventListener('click', () => {
     console.log();
     globalSearch();
-});
-
-buttonSearch.addEventListener('keydown', (e) => {
-    if (e.key === 'Enter') {
-        globalSearch();
-    }
 });
