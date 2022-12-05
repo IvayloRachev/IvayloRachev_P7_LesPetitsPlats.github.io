ae138f4160b6df39314f5ece483625ad4f5a2d39
Ivaylo Rachev <rachev.ivaylo@yahoo.fr>
2022-12-05 19:22:30 +0100

    Add open close modal
    Ajout de la fonction pour ouvrir et fermer les tags

--- scripts/tagsModal.js
+++ scripts/tagsModal.js
@@ -31,3 +31,45 @@ function tagLists(listRecipes) {
     utensilsList.innerHTML += `<ul class="tag_list">${newUtensilsList.join('')}</ul>`;
 };
 tagLists(recipes);
+//fin de tags array
+
+//open close tags modals
+const btnModal = document.querySelectorAll('.down_button');
+btnModal.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        if (e.target.classList.contains('down_ingredients')) {
+            const modalIng = document.querySelector('.ingredients_list');
+            const ingredientPlaceHolder = document.getElementsByName('ingredients');
+            ingredientPlaceHolder[0].placeholder = "Rechercher un ingrédient";
+
+            if (modalIng.classList.contains('none')) {
+                modalIng.classList.remove('none');
+            } else {
+                modalIng.classList.add('none');
+                ingredientPlaceHolder[0].placeholder = "Ingrédients";
+            }
+        } else if (e.target.classList.contains('down_utensils')) {
+            const modalDev = document.querySelector('.devices_list');
+            const devicePlaceHolder = document.getElementsByName('devices');
+            devicePlaceHolder[0].placeholder = "Rechercher un appareil";
+
+            if (modalDev.classList.contains('none')) {
+                modalDev.classList.remove('none');
+            } else {
+                modalDev.classList.add('none');
+                ingredientPlaceHolder[0].placeholder = "Appareils"; 
+            }
+        } else if (e.target.classList.contains('down_utensils')) {
+            const modalUt = document.querySelector('.utensils_list');
+            const utensilPlaceHolder = document.getElementsByName('utensils');
+            utensilPlaceHolder[0].placeholder = "Rechercher un ustensile";
+
+            if (modalUt.classList.contains('none')) {
+                modalUt.classList.remove('none');
+            } else {
+                modalUt.classList.add('none');
+                utensilPlaceHolder[0].placeholder = "Ustensiles";
+            }
+        }
+    });
+});
